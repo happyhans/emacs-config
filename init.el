9cf009baf5de755620b51c293a0e8cc9a07be8e0
Hans <11070521+happyhans@users.noreply.github.com>
2020-08-30 22:05:00 -0700

    Update init.el

--- init.el
+++ init.el
@@ -1,98 +1,90 @@
-;; emacs cheatsheet
-;;
-;; Common window commands (also called a frame in emacs)
-;; C-x 0 delete the current window
-;; C-x 1 maximize the current window
-;; C-x 2 split the current window horizontally
-;; C-x 3 split the current window vertically
-;; C-x ^ make selected window taller
-;; C-x } make selected window wider
-;; C-x - shrink window if larger than buffer
-
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
 
-;; Set internal emacs info.
-(setq user-full-name "Hans Allendorfer"
-      user-mail-address "hansallendorfer@yahoo.com")
-
-;; Highlight indentation levels.
-(add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
-(setq highlight-indent-guides-method 'column)
-
-;; Turn off menu bar.
-(menu-bar-mode -1)
-
-;; Turn off scroll bar.
-(toggle-scroll-bar -1)
+;; -----------------------------------------------------------------------------
+;; Appearance
+;; -----------------------------------------------------------------------------
+;; Line numbering, using relative line numbers.
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode)
 
 ;; Turn off tool bar.
 (tool-bar-mode -1)
 
-;; Turn off blinking cursor.
-(blink-cursor-mode -1)
-
-;; Toggle line highlighting in all buffers.
-(global-hl-line-mode +1)
-(set-face-background 'hl-line "#330")
+;; Turn off menu bar.
+(menu-bar-mode -1)
 
 ;; Show the current line and column number in the mode line.
 (line-number-mode +1)
 (column-number-mode t)
 
-;; Show line numbers in all buffers.
-(global-display-line-numbers-mode 1)
+;; Display current time.
+(display-time-mode 1)
 
+;; Theme
+(load-theme 'seoul256)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(require 'powerline)
+(powerline-default-theme)
+
+
+;; -----------------------------------------------------------------------------
+;; Usability
+;; -----------------------------------------------------------------------------
 ;; Use spaces instead of tabs when indenting.
 (setq-default indent-tabs-mode nil)
 
 ;; Default tab-size
 (setq-default tab-width 2)
 
-;; Theming
-(require 'powerline)
-(require 'moe-theme)
-(moe-theme-set-color 'orange)
-(moe-light)
-(powerline-moe-theme)
+;; ripgrep (quicker grep)
+;; Use keybinding C-c s
+(require 'rg)
+(rg-enable-default-bindings)
 
-;; Typography
-(set-face-attribute 'default nil
-                    :family "Fira Code Light"
-                    :height 160
-                    :weight 'normal
-                    :width 'normal)
+;; Company mode
+(add-hook 'prog-mode-hook 'company-mode)
+(setq company-idle-delay 0)
+(setq company-minimum-prefix-length 2)
+(setq company-show-numbers 1) ;; show quick access numbers on the left
+(setq company-selection-wrap-around t)
 
-;; Windmove settings
-;; Move from frame to frame using shift + arrow keys
+;; Flycheck
+(add-hook 'prog-mode-hook 'flycheck-mode)
+(add-hook 'ruby-mode-hook 'robe-mode)
+(eval-after-load 'company '(push 'company-robe company-backends))
+
+;; Projectile
+(projectile-mode t)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+;; Ido (interactive do)
+(require 'ido)
+(ido-mode t)
+
+;; Change "yes or no" to "y or n"
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Windmove: Shift + Arrow Keys to move windows.
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
 
-;; Ruby-related settings
-;; Files with the following extensions should open in ruby-mode
-(require 'ruby-mode)
-(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
-
-(require 'ruby-end)
-;; https://github.com/rejeep/ruby-tools.el
-(require 'ruby-tools)
+;; Magit (git interface)
+(global-set-key (kbd "C-x g") 'magit-status)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("845489fb9f7547e6348a80f942402fc7ac7c6854b0accabc49aeddd8cd4a2bd9" default))
+ '(initial-frame-alist '((fullscreen . maximized)))
  '(package-selected-packages
-   (quote
-    (company ruby-end yard-mode ruby-tools moe-theme powerline highlight-indent-guides highlight-indentation use-package))))
+   '(smart-mode-line robe flycheck magit rg company rainbow-delimiters seoul256-theme powerline)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
